77031408fb05fc9f6f4b371615447c678e3cd954
Aftab Ansari <aftab.sde9910@gmail.com>
2026-03-31 17:10:09 +0530

    Update production server URL in environment.js
    typo error fix

--- src/environment.js
+++ src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD
-  ? "https://aapnavideocllbackend.onrender.com" // future use
+  ? "https://aapnavideocallbackend.onrender.com" // future use
   : "http://localhost:8080"; // local dev backend
 
 export default server;
